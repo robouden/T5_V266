059d6a4c7f5d254fb93143633c0cdcb53ba94924
robouden <rob@yr-design.biz>
2022-06-09 16:51:20 +0900

    Init

---
+++ src/GxEPD_Example.ino
@@ -0,0 +1,180 @@
+/*
+    LilyGo Ink Screen Series Test
+        - Created by Lewis he
+*/
+
+#define LILYGO_T5_V266
+
+#include <boards.h>
+#include <GxEPD.h>
+#include <SD.h>
+#include <FS.h>
+
+#include <GxDEPG0266BN/GxDEPG0266BN.h>    // 2.66" b/w   form DKE GROUP
+
+#include GxEPD_BitmapExamples
+
+// FreeFonts from Adafruit_GFX
+#include <Fonts/FreeMonoBold9pt7b.h>
+#include <Fonts/FreeMonoBold12pt7b.h>
+#include <Fonts/FreeMonoBold18pt7b.h>
+#include <Fonts/FreeMonoBold24pt7b.h>
+#include <GxIO/GxIO_SPI/GxIO_SPI.h>
+#include <GxIO/GxIO.h>
+#include <WiFi.h>
+
+
+#define USING_SOFT_SPI
+#if defined(USING_SOFT_SPI)
+GxIO_Class io(EPD_SCLK, EPD_MISO, EPD_MOSI,  EPD_CS, EPD_DC,  EPD_RSET);
+#else
+GxIO_Class io(SPI,  EPD_CS, EPD_DC,  EPD_RSET);
+#endif
+
+GxEPD_Class display(io, EPD_RSET, EPD_BUSY);
+
+#if defined(_HAS_SDCARD_) && !defined(_USE_SHARED_SPI_BUS_)
+SPIClass SDSPI(HSPI);
+#endif
+
+void showFont(const char name[], const GFXfont *f);
+void drawCornerTest(void);
+
+bool setupSDCard(void)
+{
+#if defined(_HAS_SDCARD_) && !defined(_USE_SHARED_SPI_BUS_)
+    pinMode(SDCARD_MISO, INPUT_PULLUP);
+    SDSPI.begin(SDCARD_SCLK, SDCARD_MISO, SDCARD_MOSI);
+    return SD.begin(SDCARD_CS, SDSPI);
+#elif defined(_HAS_SDCARD_)
+    return SD.begin(SDCARD_CS);
+#endif
+    return false;
+}
+
+
+void testSpeaker()
+{
+}
+
+void testWiFi()
+{
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    // WiFi.scanNetworks will return the number of networks found
+    int n = WiFi.scanNetworks();
+    Serial.println("scan done");
+    if (n == 0) {
+        Serial.println("no networks found");
+    } else {
+        Serial.print(n);
+        Serial.println(" networks found");
+        for (int i = 0; i < n; ++i) {
+            // Print SSID and RSSI for each network found
+            Serial.print(i + 1);
+            Serial.print(": ");
+            Serial.print(WiFi.SSID(i));
+            Serial.print(" (");
+            Serial.print(WiFi.RSSI(i));
+            Serial.print(")");
+            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*");
+            delay(10);
+        }
+    }
+    Serial.println("");
+}
+
+void setup()
+{
+    bool rlst = false;
+    Serial.begin(115200);
+    Serial.println();
+    Serial.println("setup");
+
+    display.init();
+    display.setTextColor(GxEPD_BLACK);
+
+    testSpeaker();
+
+    testWiFi();
+
+    rlst = setupSDCard();
+    display.setRotation(0);
+    display.fillScreen(GxEPD_WHITE);
+    display.drawExampleBitmap(BitmapExample1, 0, 0, GxEPD_WIDTH, GxEPD_HEIGHT, GxEPD_BLACK);
+    display.setRotation(1);
+    display.setCursor(20, display.height() - 15);
+    String sizeString = "SD:" + String(SD.cardSize() / 1024.0 / 1024.0 / 1024.0) + "G";
+    display.println(rlst ? sizeString : "SD:N/A");
+    int16_t x1, x2;
+    uint16_t w, h;
+    String str = GxEPD_BitmapExamplesQ;
+    str = str.substring(2, str.lastIndexOf("/"));
+    display.getTextBounds(str, 0, 0, &x1, &x2, &w, &h);
+    display.setCursor(display.width() - w - 5, display.height() - 15);
+    display.println(str);
+    display.update();
+    delay(1000);
+}
+
+void loop()
+{
+    drawCornerTest();
+
+    int i = 0;
+    while (i < 4) {
+        display.setRotation(i);
+        showFont("FreeMonoBold9pt7b", &FreeMonoBold9pt7b);
+        //showFont("FreeMonoBold18pt7b", &FreeMonoBold18pt7b);
+        //showFont("FreeMonoBold24pt7b", &FreeMonoBold24pt7b);
+        i++;
+    }
+
+    display.fillScreen(GxEPD_WHITE);
+
+    display.update();
+
+    display.powerDown();
+
+    esp_sleep_enable_ext1_wakeup(((uint64_t)(((uint64_t)1) << BUTTON_1)), ESP_EXT1_WAKEUP_ALL_LOW);
+
+    esp_deep_sleep_start();
+
+}
+
+void showFont(const char name[], const GFXfont *f)
+{
+    display.fillScreen(GxEPD_WHITE);
+    display.setTextColor(GxEPD_BLACK);
+    display.setFont(f);
+    display.setCursor(0, 0);
+    display.println();
+    display.println(name);
+    display.println(" !\"#$%&'()*+,-./");
+    display.println("0123456789:;<=>?");
+    display.println("@ABCDEFGHIJKLMNO");
+    display.println("PQRSTUVWXYZ[\\]^_");
+    display.println("`abcdefghijklmno");
+    display.println("pqrstuvwxyz{|}~ ");
+    display.update();
+    delay(5000);
+}
+
+void drawCornerTest()
+{
+    display.drawCornerTest();
+    delay(5000);
+    uint8_t rotation = display.getRotation();
+    for (uint16_t r = 0; r < 4; r++) {
+        display.setRotation(r);
+        display.fillScreen(GxEPD_WHITE);
+        display.fillRect(0, 0, 8, 8, GxEPD_BLACK);
+        display.fillRect(display.width() - 18, 0, 16, 16, GxEPD_BLACK);
+        display.fillRect(display.width() - 25, display.height() - 25, 24, 24, GxEPD_BLACK);
+        display.fillRect(0, display.height() - 33, 32, 32, GxEPD_BLACK);
+        display.update();
+        delay(5000);
+    }
+    display.setRotation(rotation); // restore
+}
+
